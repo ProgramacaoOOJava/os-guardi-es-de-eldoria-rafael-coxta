4ba2f821e8999d4669f2896aab0b494ce67099f7
Rafael O. C. Savino <156353373+rafael-coxta@users.noreply.github.com>
2026-05-28 18:27:48 +0000

    Realizei o desafio Nível Novato: Ambiente, Classes e Objetos

--- Main.java
+++ Main.java
@@ -1,10 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
 
-        .
-        .
-        .
+        /*
+        DECLARANDO NOVOS PERSONAGENS PARA O JOGO
+         */
 
+        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 15.0);
+        Personagem heroi2 = new Personagem("Merlin", "Mago", 3, 130, 50.0);
+        Personagem heroi3 = new Personagem("Lagertha", "Valkyria", 5, 150, 75.0);
+
+        /*
+        CHAMANDO O MÉTODO DE EXIBIÇÃO DA CLASSE "Personagem"
+        */
+
+        heroi1.exibirStatus();
+        heroi2.exibirStatus();
+        heroi3.exibirStatus();
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,37 @@
 public class Personagem {
+
+    /*
+    DECLARANDO AS VARIÁVEIS RELACIONADAS AOS PERSONAAGENS DO JOGO
+    */
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
 
+    /*
+    CRIANDO MEU METODO CONSTRUTOR "Personagem"
+     */
+
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
 
-    public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
+    /* 
+    CRIANDO METODO DE EXIBIÇÃO DO STATUS DOS PERSONAGENS CRIADOS
+    */
 
+    public void exibirStatus() {
+        System.out.println("\n--- Status do Personagem ---");
+        System.out.println("Nome............: " + nome);
+        System.out.println("Classe..........: " + classe);
+        System.out.println("Nível...........: " + nivel);
+        System.out.println("Pontos de vida..: " + pontosDeVida);
+        System.out.println("Poder base......: " + poderBase);
     }
 }
 
